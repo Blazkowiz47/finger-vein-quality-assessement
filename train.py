d07dab5334636285610c30e24f5f3df321deeb41
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2023-09-16 14:05:56 +0200

    added arg parser

--- train.py
+++ train.py
@@ -2,44 +2,109 @@
 Main training file.
 calls the train pipeline with configs.
 """
+import argparse
+
 import wandb
-from common.train_pipeline.model.common_configs import (
+import yaml
+from common_configs import (
     resnet50_grapher12_conv_gelu_config,
     resnet50_grapher_attention_12_conv_gelu_config,
     grapher_attention_12_conv_gelu_config,
 )
 
+
 from common.train_pipeline.train import train
 from common.util.enums import EnvironmentType
 
-BATCH_SIZE = 10
-EPOCHS = 1000
-ENVIRONMENT = EnvironmentType.PYTORCH
-LOG_ON_WANDB = False
-RUN_NAME = "600-classes-without-augmentation"
+
+parser = argparse.ArgumentParser(
+    description="Training Config",
+    add_help=True,
+)
+parser.add_argument(
+    "-c",
+    "--config",
+    default="resnet50_grapher_attention_12_conv_gelu_config",
+    type=str,
+    help="Put model config name from common_config",
+)
+parser.add_argument(
+    "--epochs",
+    default=10_000,
+    type=int,
+    help="Add number of epochs.",
+)
+parser.add_argument(
+    "--batch-size",
+    default=10,
+    type=int,
+    help="Add batch_size.",
+)
+parser.add_argument(
+    "--environment",
+    default="pytorch",
+    type=str,
+    help="Specify environment. pytorch or tensorflow.",
+)
+parser.add_argument(
+    "--log-on-wandb",
+    type=bool,
+    default=False,
+    help="Whether to log on wandb or not.",
+)
+parser.add_argument(
+    "--wandb-run-name",
+    type=str,
+    default=None,
+    help="Wandb run name",
+)
+
+
+def get_config(config: str):
+    """
+    Fetches appropriate config.
+    """
+    if config == "resnet50_grapher12_conv_gelu_config":
+        return resnet50_grapher12_conv_gelu_config()
+    if config == "resnet50_grapher_attention_12_conv_gelu_config":
+        return resnet50_grapher_attention_12_conv_gelu_config()
+    if config == "grapher_attention_12_conv_gelu_config":
+        return grapher_attention_12_conv_gelu_config()
+
+    raise ValueError(f"Wrong config: {config}")
 
 
 def main():
     """
     Wrapper for the driver.
     """
-    if LOG_ON_WANDB:
+    args = parser.parse_args()
+    log_on_wandb = args.log_on_wandb
+    epochs = args.epochs
+    batch_size = args.batch_size
+    environment = (
+        EnvironmentType.PYTORCH
+        if args.environment == "pytorch"
+        else EnvironmentType.TENSORFLOW
+    )
+    wandb_run_name = args.wandb_run_name
+    config = get_config(args.config)
+    print(log_on_wandb, epochs, batch_size, environment, wandb_run_name, args.config)
+    if log_on_wandb:
         wandb.init(
             # set the wandb project where this run will be logged
             project="finger-vein-recognition",
-            name=RUN_NAME,
+            name=wandb_run_name,
             config={
-                "learning_rate": 0.0001,
-                "architecture": "ViG with self Attention",
-                "dataset": "CIFAR-100",
-                "epochs": EPOCHS,
+                "architecture": args.config,
+                "dataset": "Internal.",
+                "epochs": epochs,
             },
         )
-    config = grapher_attention_12_conv_gelu_config()
-    train(config, BATCH_SIZE, EPOCHS, ENVIRONMENT, LOG_ON_WANDB)
-    if LOG_ON_WANDB:
+
+    train(config, batch_size, epochs, environment, log_on_wandb)
+    if log_on_wandb:
         wandb.finish()
 
 
-if __name__ == "__main__":
-    main()
+main()
