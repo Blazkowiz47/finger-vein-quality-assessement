25f12c6f190b75ada899419909697f7f834c217c
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2023-09-28 17:40:59 +0200

    updats

--- evaluation_loop.py
+++ evaluation_loop.py
@@ -1,1 +1,21 @@
-from evaluation import evaluate
+from common.util.enums import EnvironmentType
+from common.evaluate_pipeline.evaluate import evaluate
+
+dataset_list = ["lma", "mipgan_1", "mipgan_2", "stylegan_iwbf"]
+model_type = ["train", "test"]
+def main():
+    for dataset in dataset_list:
+        for model in model_type:
+            model = f"models/checkpoints/best_{model}_vig_pyramid_{dataset}.pt"
+            try:
+                evaluate(
+                        ["dnp_"+dataset],
+                        model,
+                        512,
+                        EnvironmentType.PYTORCH,
+                        2,
+                        224,
+                        224,
+                        )
+            except:
+                ...
