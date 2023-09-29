2c86501d1d7811311e33977237831e525092e451
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2023-09-29 11:32:29 +0200

    updates

--- model_loop.py
+++ model_loop.py
@@ -10,65 +10,66 @@ model_name = "vig_attention_pyramid_tiny"
 dataset_list = ["lma", "mipgan_1", "mipgan_2", "stylegan_iwbf"]
 model_type = ["train", "test"]
 all_results = {}
-def main():
-    for dataset in dataset_list:
-        wandb_run_name = f"{model_name}_{dataset}"
-        act = 'gelu'
-        pred_type = 'conv'
-        epochs = 25 
-        n_classes = 2
-        height = 224
-        width = 224
-        batch_size = 192 
-        validate_after_epochs = 5
-        learning_rate = 1e-4
-        num_heads = 2
-        augment_times = 19
-        config = get_config(
-            model_name,
-            act,
-            pred_type,
-            n_classes,
-            num_heads,
-            height,
-            width,
-        )
-        
-        if wandb_run_name:
-            wandb.init(
-                # set the wandb project where this run will be logged
-                project="finger-vein-recognition",
-                name=wandb_run_name,
-                config={
-                    "architecture": model_name,
-                    "dataset": dataset,
-                    "epochs": epochs,
-                    "activation":act ,
-                    "predictor_type": pred_type,
-                },
-            )
-        try:
-            train(
-                config,
-                "dnp_" + dataset,
-                batch_size,
-                epochs,
-                EnvironmentType.PYTORCH,
-                wandb_run_name,
-                validate_after_epochs,
-                learning_rate,
-                None,
-                augment_times,
+def main(train_models:bool = False):
+    if train_models:
+        for dataset in dataset_list:
+            wandb_run_name = f"{model_name}_{dataset}"
+            act = 'gelu'
+            pred_type = 'conv'
+            epochs = 25 
+            n_classes = 2
+            height = 224
+            width = 224
+            batch_size = 192 
+            validate_after_epochs = 5
+            learning_rate = 1e-4
+            num_heads = 2
+            augment_times = 19
+            config = get_config(
+                model_name,
+                act,
+                pred_type,
                 n_classes,
+                num_heads,
                 height,
                 width,
             )
-        except KeyboardInterrupt:
-            pass
+            
+            if wandb_run_name:
+                wandb.init(
+                    # set the wandb project where this run will be logged
+                    project="finger-vein-recognition",
+                    name=wandb_run_name,
+                    config={
+                        "architecture": model_name,
+                        "dataset": dataset,
+                        "epochs": epochs,
+                        "activation":act ,
+                        "predictor_type": pred_type,
+                    },
+                )
+            try:
+                train(
+                    config,
+                    "dnp_" + dataset,
+                    batch_size,
+                    epochs,
+                    EnvironmentType.PYTORCH,
+                    wandb_run_name,
+                    validate_after_epochs,
+                    learning_rate,
+                    None,
+                    augment_times,
+                    n_classes,
+                    height,
+                    width,
+                )
+            except KeyboardInterrupt:
+                pass
 
-        if wandb_run_name:
-            wandb.finish()
-            torch.cuda.empty_cache()
+            if wandb_run_name:
+                wandb.finish()
+                torch.cuda.empty_cache()
 
     print("Starting evaluation")
 
@@ -77,12 +78,12 @@ def main():
         all_results[model_name]["best" + model_t] = {}
         for dataset_model in dataset_list:
             model = f"models/checkpoints/best_{model_t}_{model_name}_{dataset_model}.pt"
-            all_results[model_name]["best" + model_t] = {}
+            all_results[model_name]["best" + model_t][dataset_model] = {}
             for dataset in dataset_list:
                 try:
                     print("Model:", model)
                     print("Dataset:", dataset)
-                    all_results[model_name]["best" + model_t][dataset] = evaluate(
+                    all_results[model_name]["best" + model_t][dataset_model][dataset] = evaluate(
                             ["dnp_"+dataset],
                             model,
                             512,
